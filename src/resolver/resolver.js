00a224de4aa83388b5b87fa672de3739e5b49ab6
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-02-17 11:24:42 +0500

    feat: added transaction mutations

--- src/resolver/resolver.js
+++ src/resolver/resolver.js
@@ -10,23 +10,238 @@ export default {
       try {
         let { amount } = args.input;
         let { Transactions } = context.collections;
-        console.log("collections are ");
-        console.log(context.collections);
-        console.log("transactions are ");
-        console.log(Transactions);
-        let createdTransaction = await Transactions.insertOne({ amount });
+        let { auth, authToken, userId } = context;
+
+        if (!authToken) {
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unauthorized",
+              status: 401,
+            },
+          };
+        }
+
+        if (!userId) {
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unauthorized",
+              status: 200,
+            },
+          };
+        }
+
+        let data = {
+          amount,
+          isApproved: false,
+          transactionBy: userId,
+        };
+        let createdTransaction = await Transactions.insertOne(data);
         console.log("Created transaction is ", createdTransaction);
         return {
-          success: true,
-          message: "Transaction Created",
-          status: 200,
+          transactionOutput: {
+            success: true,
+            message: "Transaction Created",
+            status: 200,
+          },
         };
       } catch (err) {
         console.log("error is ", err);
         return {
-          success: false,
-          message: `Server Error ${err}`,
-          status: 500,
+          transactionOutput: {
+            success: false,
+            message: `Server Error ${err}`,
+            status: 500,
+          },
+        };
+      }
+    },
+    async deleteTransaction(parent, args, context, info) {
+      try {
+        let { transactionId } = args;
+        let { authToken, collections } = context;
+        let { Transactions } = collections;
+        // let decodedProductId = decodeOpaqueId(productId)?.id
+        if (!authToken) {
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unauthorized",
+              status: 401,
+            },
+          };
+        }
+        let data = {
+          _id: ObjectID.ObjectId(transactionId),
+        };
+
+        let updatedTransaction = await Transactions.remove(data);
+        console.log(
+          "deletedLicense",
+          deletedTransaction,
+          deletedTransaction?.result?.n
+        );
+        if (deletedTransaction?.result?.n > 0)
+          return {
+            transactionOutput: {
+              success: true,
+              message: "Successfully Deleted",
+              status: 200,
+            },
+          };
+        else
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unable to delete",
+              status: 200,
+            },
+          };
+      } catch (err) {
+        return {
+          transactionOutput: {
+            success: false,
+            message: `Server Error ${err}`,
+            status: 500,
+          },
+        };
+      }
+    },
+    async approveTransaction(parent, args, context, info) {
+      try {
+        let { transactionId } = args;
+        let { authToken, collections } = context;
+        let { Transactions } = collections;
+        if (!authToken) {
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unauthorized",
+              status: 401,
+            },
+          };
+        }
+
+        let approvedTransaction = await Transactions.findAndUpdateOne(
+          {
+            _id: transactionId,
+          },
+          { $set: { isApproved: true } }
+        );
+        console.log(approvedTransaction, approvedTransaction?.result?.n);
+        if (approvedTransaction?.result?.n > 0)
+          return {
+            transaction: approvedTransaction,
+            transactionOutput: {
+              success: true,
+              message: "Transaction Approved",
+              status: 200,
+            },
+          };
+        else
+          return {
+            transactionOutput: {
+              success: false,
+              message: "Unable to approve transaction",
+              status: 200,
+            },
+          };
+      } catch (err) {
+        return {
+          transactionOutput: {
+            success: false,
+            message: `Server Error ${err}`,
+            status: 500,
+          },
+        };
+      }
+    },
+  },
+  Query: {
+    async getAllTransactions(parents, args, context, info) {
+      try {
+        let { collections } = context;
+        let { Transactions } = collections;
+        let allTransactions = await Transactions.find().toArray();
+        if (allTransactions?.length) {
+          return {
+            transactions: allTransactions,
+            response: {
+              success: true,
+              message: "Transactions Found",
+              status: 200,
+            },
+          };
+        } else {
+          return {
+            transactions: null,
+            response: {
+              success: false,
+              message: "No Transactions Found",
+              status: 200,
+            },
+          };
+        }
+      } catch (err) {
+        console.log(err);
+        return {
+          transactions: null,
+          response: {
+            success: false,
+            message: `Server Error ${err}`,
+            status: 500,
+          },
+        };
+      }
+    },
+    async getUserTransactions(parents, args, context, info) {
+      try {
+        let { auth, authToken, userId, collections } = context;
+        let { Transactions } = collections;
+
+        if (!authToken) {
+          return {
+            transactions: null,
+            response: {
+              success: false,
+              message: "Unauthorized",
+              status: 401,
+            },
+          };
+        }
+        let userTransactions = await Transactions.find({
+          transactionBy: userId,
+        }).toArray();
+        console.log("user transactions are");
+        console.log(userTransactions);
+        if (userTransactions?.length) {
+          return {
+            transactions: userTransactions,
+            response: {
+              success: true,
+              message: "Transactions Found",
+              status: 200,
+            },
+          };
+        } else {
+          return {
+            transactions: userTransactions,
+            response: {
+              success: false,
+              message: "No Transactions Found",
+              status: 200,
+            },
+          };
+        }
+      } catch (err) {
+        return {
+          transactions: null,
+          response: {
+            success: false,
+            message: `Server Error ${err}`,
+            status: 500,
+          },
         };
       }
     },
